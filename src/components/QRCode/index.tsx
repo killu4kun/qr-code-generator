a07d5ea0ded977ebf56a5725c1a39f3295866666
matheus.soares2 <matheus.soares@finderup.com.br>
2023-03-15 11:34:29 -0300

    feat : mobile proportions

--- src/components/QRCode/index.tsx
+++ src/components/QRCode/index.tsx
@@ -1,7 +1,7 @@
 import QRCode from "react-qr-code";
 import { useNavigate } from "react-router-dom";
 import useQrCode from "../../hooks/useQrCode";
-import { ArrowRightIcon } from "@heroicons/react/24/solid";
+import { ArrowDownIcon } from "@heroicons/react/24/solid";
 
 export default function QRCd() {
   const { name, linkedin, github } = useQrCode();
@@ -16,20 +16,20 @@ export default function QRCd() {
   };
 
   return (
-    <div className="h-screen bg-gray-50 flex items-center w-screen">
+    <div className="flex h-screen bg-gray-50  items-center w-screen">
       <section className="bg-cover bg-center py-32 w-full bg-[#F4F0DB] ">
         <div className="container mx-auto text-left text-white">
-          <div className="flex items-center ">
-            <div className="w-1/3 text-[#ECD9BA]">
+          <div className="md:flex items-center sm:flex flex-col flex">
+            <div className="flex flex-col justify-center items-center w-1/3 text-[#ECD9BA]">
               <h1 className="text-5xl font-medium mb-6 ">{name}</h1>
               <p className="text-xl mb-12">SCAN ME</p>
             </div>
-            <div className="w-1/3">
-              <ArrowRightIcon className="sm:h-20 h-10 w-20 text-[#ECD9BA]" />
+            <div className="flex justify-center w-full">
+              <ArrowDownIcon className="sm:h-40 h-40 w-20 text-[#ECD9BA]" />
             </div>
             <div className="flex grow-1">
               {window.innerWidth <= 768 ? (
-                <QRCode onClick={handleQrCodeClick} value={newURL} size={75} />
+                <QRCode onClick={handleQrCodeClick} value={newURL} size={100} />
               ) : (
                 <QRCode onClick={handleQrCodeClick} value={newURL} size={100} />
               )}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -13,26 +13,19 @@ export default function Profile() {
 
   return (
     <div className="h-screen bg-gray-50 flex items-center">
-      <section className="bg-cover bg-center py-32 w-full bg-black">
+      <section className="flex flex-col bg-cover bg-center py-32 w-full bg-black">
         <div className="container mx-auto text-left text-white">
-          <div className="flex items-center">
+          <div className="flex items-center flex-col">
             <div className="w-1/2">
               <h1 className="text-5xl font-medium mb-6">{name}</h1>
               <p className="text-xl mb-12">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
                 viverra euismod odio, gravida pellentesque urna varius vitae.
               </p>
-              <div className="flex w-[100%] justify-around">
+              <div className="flex w-[100%] justify-around gap-8">
                 <Anchor link={linkedin || LINKEDIN_URL} label="Linkedin" />
                 <Anchor link={github || GITHUB_URL} label="Github" />
               </div>
-            </div>
-            <div className="w-1/2 pl-16">
-              <img
-                src="https://source.unsplash.com/random?ux"
-                className="h-64 w-full object-cover rounded-xl"
-                alt="Layout Img"
-              />
             </div>
           </div>
         </div>
